7f443da244d9412c4c06e36febd3ab3931925c94
Kollc <46793445+Kollc@users.noreply.github.com>
2019-03-06 23:10:58 +0300

    corrected errors in the header + added js for the price menu in the catalog

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-
-
+//ПЕРЕМЕННЫЕ ДЛЯ ПОПАБА ПИСЬМА В INDEX
 var buttonCallLetterElemet = document.getElementById('button-call-letter');
 var allModulLetter = document.getElementById('all-modul-letter');
 var allModulLetterCloseButton = document.getElementById('modul-close-catalog');
 
+//ПЕРЕМЕННЫЕ ДЛЯ КАРТЫ В INDEX
 var linksCallMapElement = document.getElementById('links-call-map');
 var allModulMap = document.getElementById('all-modul-map');
 var allModulMapCloseButton = document.getElementById('modul-close-catalog-map');
 
+//ПЕРЕМЕННЫЕ ДЛЯ ПРАЙСА В КАТАЛОГЕ
+var area = document.querySelector('.catalog-item');
+var imgCatalog = document.querySelector('.catalog-item-img');
+var menuPrice = document.querySelector('.catalog-item-buy-grid');
 
+//КОД ПИСЬМА В INDEX
 if (buttonCallLetterElemet) {
   buttonCallLetterElemet.addEventListener('click', function(e) {
     allModulLetter.classList.toggle('all-modul-letter_hidden');
@@ -22,18 +27,15 @@ if (allModulLetterCloseButton) {
   })
 }
 
-//////////////////////////////////////////
-if(linksCallMapElement)
-{
-  linksCallMapElement.addEventListener('click' , function(evnt)
-  {
+//КОД ДЛЯ КАРТЫ В INDEX
+if (linksCallMapElement) {
+  linksCallMapElement.addEventListener('click', function(evnt) {
     event.preventDefault();
     allModulMap.classList.toggle('all-modul-map_hidden');
   })
 }
 
-if(allModulMapCloseButton)
-{
+if (allModulMapCloseButton) {
   allModulMapCloseButton.addEventListener('click', function(eventt) {
     event.preventDefault();
     allModulMap.classList.toggle('all-modul-map_hidden');
@@ -41,8 +43,23 @@ if(allModulMapCloseButton)
 }
 
 
-////////
+//КОД ДЛЯ ПРАЙСА В КАТАЛОГЕ
 
+if (area) {
+  area.addEventListener('mouseover', function(event) {
+    //  event.preventDefault();
+    imgCatalog.classList.add('catalog-item-img_hidden');
+    menuPrice.classList.add('catalog-item-buy-grid_hidden');
+
+  })
+
+  area.addEventListener('mouseout', function(event) {
+    imgCatalog.classList.remove('catalog-item-img_hidden');
+    menuPrice.classList.remove('catalog-item-buy-grid_hidden');
+  })
+}
+
+//КОД ДЛЯ ВАЛИКА..НЕ ГОТОВЫЙ
 var inputRangeElement = document.querySelector('input[type="range"]');
 var resultRangePrice = document.querySelector('.resulr-range-price');
 
