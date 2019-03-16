54db4caca23e33af11df0651204e678201cecfbe
Kollc <46793445+Kollc@users.noreply.github.com>
2019-03-16 03:03:07 +0300

    corrected errors

--- js/script.js
+++ js/script.js
@@ -20,51 +20,6 @@ var back = document.querySelector('.gallery-button-back');
 var slide = document.querySelector('.slider-content-2');
 
 
-
-//КОД ПИСЬМА В INDEX
-buttonCallLetterElemet.addEventListener('click', function(e) {
-  allModulLetter.classList.add('hidden');
-});
-
-allModulLetterCloseButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  allModulLetter.classList.remove('hidden');
-});
-
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (allModulLetter.classList.contains('hidden')) {
-      allModulLetter.classList.remove('hidden');
-    }
-  }
-});
-
-
-//КОД ДЛЯ КАРТЫ В INDEX
-linksCallMapElement.addEventListener('click', function(evnt) {
-  event.preventDefault();
-  allModulMap.classList.add('hidden-map');
-});
-
-
-allModulMapCloseButton.addEventListener('click', function(eventt) {
-  event.preventDefault();
-  allModulMap.classList.remove('hidden-map');
-});
-
-
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (allModulMap.classList.contains('hidden-map')) {
-      allModulMap.classList.remove('hidden-map');
-    }
-  }
-});
-
-
-
 //КОД ДЛЯ ПОПАБА ПРАЙС МЕНЮ
 document.addEventListener('click', function(event) {
   if (event.target.className === 'button-buy') {
@@ -106,5 +61,52 @@ if (back) {
   back.addEventListener('click', function(event) {
     event.preventDefault();
     slide.classList.remove('slider-content-2_hidden');
+  });
+}
+
+//КОД ПИСЬМА В INDEX
+if (buttonCallLetterElemet) {
+  buttonCallLetterElemet.addEventListener('click', function(e) {
+    e.preventDefault();
+    allModulLetter.classList.add('hidden');
+  });
+
+  allModulLetterCloseButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    allModulLetter.classList.remove('hidden');
+  });
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (allModulLetter.classList.contains('hidden')) {
+        allModulLetter.classList.remove('hidden');
+      }
+    }
+  });
+}
+
+
+//КОД ДЛЯ КАРТЫ В INDEX
+if (linksCallMapElement) {
+  linksCallMapElement.addEventListener('click', function(evnt) {
+    event.preventDefault();
+    allModulMap.classList.add('hidden-map');
+  });
+
+
+  allModulMapCloseButton.addEventListener('click', function(eventt) {
+    event.preventDefault();
+    allModulMap.classList.remove('hidden-map');
+  });
+
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (allModulMap.classList.contains('hidden-map')) {
+        allModulMap.classList.remove('hidden-map');
+      }
+    }
   });
 }
